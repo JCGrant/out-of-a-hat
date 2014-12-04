657ebc6a4e024d4d3c949f2d78abd14f28b7a66e
James Grant <jamescolin.grant@gmail.com>
2014-12-04 12:46:08 +0000

    Renamed SERVER_NAME to URL. SERVER_NAME just isn't the correct config variable to use, so I have created my own

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 if os.environ.get('DATABASE_URL') is None:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir, 'app.db')
-    SERVER_NAME = '127.0.0.1:5000'
+    URL = '127.0.0.1:5000'
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    SERVER_NAME = 'http://out-of-a-hat.herokuapp.com:80'
+    URL = 'http://out-of-a-hat.herokuapp.com'
