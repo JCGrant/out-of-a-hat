592dd71bef82a490e7b7d41f759540ff722c9f09
James Grant <jamescolin.grant@gmail.com>
2014-12-04 13:13:36 +0000

    Trying to get app to run on heroku

--- run.py
+++ run.py
@@ -4,7 +4,8 @@ import os
 
 from out_of_a_hat import app
 
-if os.environ.get('DATABASE_URL') is None:
-    app.run(debug=True, host='0.0.0.0')
-else:
-    app.run()
+if __name__ == '__main__':
+    if os.environ.get('DATABASE_URL') is None:
+        app.run(debug=True, host='0.0.0.0')
+    else:
+        app.run()
